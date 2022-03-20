806dbcc573f22d77a04103bb8a7d1f53d3d59cfe
BHDShostak <BHDShostak@github.com>
2022-03-20 15:40:29 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+# Program for calculating text and numbers in which you need to remove every third character
+
+
+def kill3charter(txt):
+    OutText = ''
+    for i in range (0,l):
+        if i % 3 != 0:
+            OutText += txt[i]
+    return OutText
+
+txt = input () #input the text, who need a delete every 3-th charachter
+
+l = len (txt) #count a long of text
+
+print(kill3charter(txt)) # output text to consumer
+
+
